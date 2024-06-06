6165732c6aa2b9b0cd26cbf2387a366bc4f3e58b
Samer Alnajjar <samerk.alnajjar@gmail.com>
2024-06-06 14:55:49 +0000

    create a dropdown from the zero

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 
-import './index.css';
+import './index.css'
+
 
 import ButtonPage from './pages/ButtonPage';
 import AccordionPage from './pages/AccordionPage';
+import DropdownPage from './pages/DropwdownPage';
 
 const App = () => {
 
+
+
+
     return (
         <div >
-            < ButtonPage />
+            <ButtonPage />
             <AccordionPage />
+            <DropdownPage />
         </div >)
 }
 
