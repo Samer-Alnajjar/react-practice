a7f3aedb1d05408efa3be3de50e8f42b2bf017cf
Samer Alnajjar <samerk.alnajjar@gmail.com>
2024-06-30 18:51:54 +0000

    Create a sort feature for the table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,20 +1,22 @@
+
+
+
 export default function Table({ tableConfig, tableData }) {
-    const renderHeaders = tableConfig.map(header => <th className="p-4 pb-2" key={header.label}>{header.label}</th>);
+
     const renderRows = tableData.map(data => {
-        const row = Object.entries(data).map(prop => <td className="p-4 text-center	" key={prop[1]}>{prop[1]}</td>)
+        const rows = Object.entries(data).map(prop => <td className="p-4 text-center" key={prop[1]}>{prop[1]}</td>)
         return (
-            <tr key={data.label} className="border-b p-4 border-gray-300 ">
-                {row}
+            <tr key={data.label} className="border-b p-4 border-gray-300 flex justify-between">
+                {rows}
             </tr>
         )
-
     });
 
     return (
-        <table className="table-auto border-spacing-2">
+        <table className="table-auto border-spacing-2 ">
             <thead >
-                <tr className="border-b-2 border-gray-300">
-                    {renderHeaders}
+                <tr className="border-b-2 border-gray-300 flex">
+                    {tableConfig}
                 </tr>
             </thead>
             <tbody >
